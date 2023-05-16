87a966403cdf77ba440d40df0c2f48345eb25f9d
Masonb18 <bauer.mason01@gmail.com>
2023-05-16 09:21:08 -0500

    connecting the server.js to the routes

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 const express= require('express');
+const path= require('path');
+const fs= require('fs');
 const app = express();
+const PORT= process.env.PORT || 3001;
 
-app.get('/', (req,res)=>{
-    console.log('Here')
+app.use(express.static('public'))
+app.use(express.json())
+
+
+app.get('/notes', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) =>{
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, data)=>{
+        if (err){
+            console.log(err)
+            res.json('could not read file')
+        }else {
+            res.json(JSON.parse(data))
+        }
+    })
+})
+
+app.post('/api/notes', (req, res) =>{
+    console.log(req.body)
+})
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.listen(PORT, () =>{
+    console.log('server running on' + PORT)
 })
